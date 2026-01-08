4fe28855c6cc4d236045ac1dccf7b14c5d6634c3
erikbeauvalot-sg <erik@beauvalot.com>
2026-01-08 14:52:36 +0100

    fix(backend): Fix backend/models/index.js to properly export root models
    - Changed backend/models/index.js to re-export from root models/ directory
    - Previous implementation tried to load models from empty backend/models/ directory
    - This caused db.User and other models to be undefined, resulting in 500 errors
    
    Fixes: 'Cannot read properties of undefined (reading findOne)' login error

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,40 +1,8 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/database.js')[env];
-const db = {};
+/**
+ * Backend models index
+ * 
+ * This file re-exports the root models directory.
+ * The actual model definitions are in /models/
+ */
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  // For SQLite, pass only the options object. For PostgreSQL, pass database, username, password, and options.
-  if (config.dialect === 'sqlite') {
-    sequelize = new Sequelize(config);
-  } else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
-  }
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+module.exports = require('../../models');
