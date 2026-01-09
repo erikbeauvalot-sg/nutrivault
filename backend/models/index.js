5890d181e13f8d82ca2050804331e8d697dde663
erikbeauvalot-sg <erik@beauvalot.com>
2026-01-09 10:57:26 +0100

    Init empty repo

--- backend/models/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Backend models index
- * 
- * This file re-exports the root models directory.
- * The actual model definitions are in /models/
- */
-
-module.exports = require('../../models');
